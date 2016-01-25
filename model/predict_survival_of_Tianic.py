798fede7df7fe595b7a0d7e8871c0292b9d19bb2
zhmi <zmmingtiandege1314@126.com>
2016-01-25 23:13:20 +0800

    1.If all the female survived and all the male did not survived ,the acurancy  rate is 0.76555. 2.If predict the survived life by the model in the codes,the acurancy rate is 0.76077.

--- model/predict_survival_of_Tianic.py
+++ model/predict_survival_of_Tianic.py
@@ -16,13 +16,14 @@
 #                   survived filed additionally.
 # Author:           zhmi
 # E-mail:           zhmi120@sina.com
-# Create:           2016-1-24
-# Recent-changes:   2016-1-24
+# Create:           2016-1-25
+# Recent-changes:   2016-1-25
 
 # <codecell>
 
 import string
 import math
+import csv
 
 # <codecell>
 
@@ -44,7 +45,6 @@ def readFile(path):
 
 def filterDataByCategory(data_list, category_id):
     category_list = map(lambda x: x[category_id],data_list)
-    #category_list = map(lambda x: float(x),category_list[1:])
     category_list = map(lambda x: int(math.floor(float(x))),category_list[1:])
     return category_list
 
@@ -56,7 +56,34 @@ def mergeCategoryWithSurvived(survived_list,category_list):
 
 # <codecell>
 
-configVariables()
+def predictSurvivedBySex(survived_num,passenger_id_list,sex_list):
+    passenger_id_sex_list = map(None,passenger_id_list,sex_list)
+    passenger_id_survived_list = []
+    
+    passenger_id_sex_list.sort(key = lambda x: x[1])
+    passenger_id_survived_list \
+    = map(lambda x: (x[0],1),passenger_id_sex_list[-survived_num:])\
+    + map(lambda x: (x[0],0),passenger_id_sex_list[:-survived_num])
+        
+    passenger_id_survived_list.sort(key = lambda x:x[0])  
+    return passenger_id_survived_list
+
+# <codecell>
+
+def predictSurvivedByAge(passenger_id_list,sex_list):
+    pass
+
+# <codecell>
+
+def writeFile(file_name,data_list):
+    csvfile = file(file_name, 'wb')
+    writer = csv.writer(csvfile)
+    writer.writerow(['PassengerId','Survived'])
+    writer.writerows(data_list)
+    csvfile.close()
+
+# <codecell>
+
 train_data_list = readFile(train_data_path)
 
 category_record_list = []
@@ -81,18 +108,22 @@ survived_num = len(filter(lambda x: x == 1,category_record_list[0])) #342
 survived_probability = float(survived_num)/total_num #0.383838383838
 print "survived_probability:",survived_probability
 
-# <codecell>
-
 # P(survived|male);P(survived|female) 
 survived_male_num = len(filter(lambda x: x[0] == 1 and x[1] == 0,survived_category_record_list[0])) #109
 survived_female_num = len(filter(lambda x: x[0] == 1 and x[1] == 1,survived_category_record_list[0])) #233
-survived_male_probability = float(survived_male_num)/survived_num
-survived_female_probability = float(survived_female_num)/survived_num
 
-print "survived_male_probability:",survived_male_probability #0.318713450292
-print "survived_female_probability:",survived_female_probability #0.681286549708
+male_num = len(filter(lambda x: x[1] == 0,survived_category_record_list[0]))
+female_num = len(filter(lambda x: x[1] == 1,survived_category_record_list[0]))
 
-# <codecell>
+survived_male_probability = float(survived_male_num)/male_num
+survived_female_probability = float(survived_female_num)/female_num 
+
+print "survived_male_probability:",survived_male_probability 
+print "survived_female_probability:",survived_female_probability 
+
+# survived_male_probability: 0.188908145581
+# survived_female_probability: 0.742038216561
+
 
 # P(survived|Age)
 # child:0——9;   juvenile:10——17   youth:18——34 middle-age:35—59 old-people:60
@@ -102,31 +133,61 @@ survived_youth_num = len(filter(lambda x: x[0] == 1 and x[1] >= 18 and x[1] <= 3
 survived_middle_age_num = len(filter(lambda x: x[0] == 1 and x[1] >= 35 and x[1] <= 59,survived_category_record_list[1])) #87
 survived_old_people_num = len(filter(lambda x: x[0] == 1 and x[1] >= 60,survived_category_record_list[1])) #7
 
-survived_child_probability = float(survived_child_num)/survived_num
-survived_juvenile_probability = float(survived_juvenile_num)/survived_num
-survived_youth_probability = float(survived_youth_num)/survived_num
-survived_middle_age_probability = float(survived_middle_age_num)/survived_num
-survived_old_people_probability = float(survived_old_people_num)/survived_num
+child_num = len(filter(lambda x: x[1] <= 9,survived_category_record_list[1])) 
+juvenile_num = len(filter(lambda x: x[1] >= 10 and x[1] <= 17,survived_category_record_list[1])) 
+youth_num = len(filter(lambda x: x[1] >= 18 and x[1] <= 34,survived_category_record_list[1])) 
+middle_age_num = len(filter(lambda x: x[1] >= 35 and x[1] <= 59,survived_category_record_list[1])) 
+old_people_num = len(filter(lambda x: x[1] >= 60,survived_category_record_list[1])) 
 
+survived_child_probability = float(survived_child_num)/child_num
+survived_juvenile_probability = float(survived_juvenile_num)/juvenile_num
+survived_youth_probability = float(survived_youth_num)/youth_num
+survived_middle_age_probability = float(survived_middle_age_num)/middle_age_num
+survived_old_people_probability = float(survived_old_people_num)/old_people_num
 
-print "survived_child_probability:",survived_child_probability
-print "survived_juvenile_probability:",survived_juvenile_probability
-print "survived_youth_probability:",survived_youth_probability
-print "survived_middle_age_probability:",survived_middle_age_probability
-print "survived_old_people_probability:",survived_old_people_probability
+'''
+survived_child_probability: 0.612903225806
+survived_juvenile_probability: 0.450980392157
+survived_youth_probability: 0.345018450185
+survived_middle_age_probability: 0.414285714286
+survived_old_people_probability: 0.269230769231
+'''
 
-# <codecell>
 
 # P(survived|Fare)
+# third-class-fare: 0--12 second-class-fare: 13--29 first-class_fare: >=30
 
-# <codecell>
+survived_third_class_num = len(filter(lambda x: x[0] == 1 and x[1] <= 12 ,survived_category_record_list[2]))
+survived_second_class_num = len(filter(lambda x: x[0] == 1 and x[1] <= 29 and x[1]>= 13 ,survived_category_record_list[2]))
+survived_first_class_num = len(filter(lambda x: x[0] == 1 and x[1] >= 30 ,survived_category_record_list[2]))
+
+third_class_num = len(filter(lambda x: x[1] <= 12 ,survived_category_record_list[2]))
+second_class_num = len(filter(lambda x: x[1] <= 29 and x[1]>= 13 ,survived_category_record_list[2]))
+first_class_num = len(filter(lambda x: x[1] >= 30 ,survived_category_record_list[2]))
+
+survived_third_class_probalibity = float(survived_third_class_num)/third_class_num
+survived_second_class_probalibity = float(survived_second_class_num)/second_class_num
+survived_first_class_probalibity = float(survived_first_class_num)/first_class_num
+
+'''
+survived_third_class_probalibity: 0.233160621762
+survived_second_class_probalibity: 0.418867924528
+survived_first_class_probalibity: 0.5875
+'''
 
 
-# <codecell>
 
+test_data_list = readFile(test_data_path)
+# [['PassengerId', 'Sex', 'Age', 'Fare'],...]
 
+test_category_record_list = []
+for i in xrange(3):
+    test_category_record_list.append(filterDataByCategory(test_data_list,i))
 
+test_data_num = len(test_category_record_list[0])
+survived_num = int(test_data_num * survived_probability)#160
 
-# <codecell>
+passenger_id_survived_list = predictSurvivedBySex(survived_num,test_category_record_list[0],test_category_record_list[1])
+writeFile('predict_by_sex',passenger_id_survived_list)
 
 
